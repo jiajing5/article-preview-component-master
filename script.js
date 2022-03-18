10dbef6a53e4a2d7b48c6a06a637365e965a522d
jiajing <a503my@gmail.com>
2022-03-18 15:01:03 +0800

    提交html css js

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleshow(){
+  document.getElementById('dialogbox').classList.toggle('hide');
+  document.getElementById('shareiconbk').classList.toggle('changeshareiconbkcolor');
+  document.getElementById('shareicon').classList.toggle('shareiconturnwhite');
+}
